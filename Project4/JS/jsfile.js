9dd7378cf8c85e1133155236510b2a1b00c94660
Prem Kumar Kardale <premkumarkardale@gmail.com>
2024-06-23 19:51:21 +0530

    initial commit

---
+++ Project4/JS/jsfile.js
@@ -0,0 +1,12 @@
+<script>
+    let hide = document.getElementById('continue');
+    console.log(hide);
+    function toggleHide() {
+            if (hide.style.display != 'none') {
+        hide.style.display = 'none';
+            }
+    else {
+        hide.style.display = 'block';
+            }
+        }
+</script>

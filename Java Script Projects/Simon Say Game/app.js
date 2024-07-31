dfa93d00f7b30966a7b10de11fa528900c98fce3
premkumar-004 <premkumarkardale@gmail.com>
2024-07-31 17:29:29 +0530

    completed Simon Say Game

--- Java Script Projects/Simon Say Game/app.js
+++ Java Script Projects/Simon Say Game/app.js
@@ -30,6 +30,7 @@ function userFlash(btn) {
 }
 
 function levelUp() {
+    userSeq = [];
     level++;
     h2.innerText = `Level ${level}`;
 
@@ -42,13 +43,40 @@ function levelUp() {
     gameFlash(randBtn);
 }
 
+function checkAns(idx) {
+    if (userSeq[idx] === gameSeq[idx]) {
+        if (userSeq.length == gameSeq.length) {
+            setTimeout(levelUp, 1000);
+        }
+    }
+    else {
+        h2.innerHTML = `Game Over! Your score was <b>${level}</b><br>Press any key to start`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function () {
+            document.querySelector("body").style.backgroundColor = "white";
+        }, 150);
+        reset();
+    }
+}
+
 function btnPress() {
-    console.log(this);
     let btn = this;
     userFlash(btn);
+
+    let userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+
+    checkAns(userSeq.length - 1);
 }
 
 let allBtns = document.querySelectorAll(".btn");
 for (btn of allBtns) {
     btn.addEventListener("click", btnPress);
+}
+
+function reset() {
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
 }

27c414021eb8f50bfb4e475a3cc8ccf15b7a9510
premkumar-004 <premkumarkardale@gmail.com>
2024-08-16 19:08:14 +0530

    Final commit : Website working properly and any news query can be searched and if clicked on the card it will redirect you to the actual news website where the article was written

--- News Application/js/app.js
+++ News Application/js/app.js
@@ -23,4 +23,38 @@ function fillDataInCard(cardClone, article) {
     const newsTitle = cardClone.querySelector("#news-title");
     const newsSource = cardClone.querySelector("#news-source");
     const newsDesc = cardClone.querySelector("#news-desc");
+
+    newsImg.src = article.urlToImage;
+    newsTitle.innerHTML = article.title;
+    newsDesc.innerHTML = article.description;
+    // newsSource.innerHTML = article.source.name;
+
+    const date = new Date(article.publishedAt).toLocaleString("en-US", {
+        timeZone: "Asia/Jakarta"
+    })
+
+    newsSource.innerHTML = `${article.source.name} . ${date}`;
+
+    cardClone.firstElementChild.addEventListener("click", () => {
+        window.open(article.url, "_blank");
+    })
 }
+let currSelectedNav = null;
+function onNavItemClick(id) {
+    fetchNews(id);
+    const navItem = document.getElementById(id);
+    currSelectedNav?.classList.remove('active');
+    currSelectedNav = navItem;
+    currSelectedNav.classList.add('active');
+}
+
+const searchButton = document.getElementById('search-button');
+const searchText = document.getElementById('news-input');
+
+searchButton.addEventListener("click", () => {
+    const query = searchText.value;
+    if (!query) return;
+    fetchNews(query);
+    currSelectedNav?.classList.remove('active');
+    currSelectedNav = null;
+})
